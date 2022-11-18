9d9389dbf30c63fca6895e33e7d2e5f53b76c9f2
t0uu <srtouzet@hotmail.com>
2022-11-18 13:17:07 -0300

    fixed error pokeApi

--- api/pokeApi.ts
+++ api/pokeApi.ts
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
+
 const pokeApi = axios.create({
     baseURL: 'https://pokeapi.co/api/v2'
-})
+});
 
-
-
-pokeApi.get('/pokemon?limit=151')
 
 
 export default pokeApi;
-
